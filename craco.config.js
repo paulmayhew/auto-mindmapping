ded6cc701b054bfe10a779c1fe0240aed6390234
whycookierocket <p.allen.mayhew@gmail.com>
2025-05-02 16:39:38 -0500

    Remove React Refresh from production builds
    Modified `craco.config.js` to filter out React Refresh plugin in production environment to improve build performance. Added a new `build:prod` script in `package.json` to explicitly set `NODE_ENV` to production and disable Fast Refresh during production builds.

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,28 @@ module.exports = {
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
+        configure: (webpackConfig, { env, paths }) => {
+            // Remove React Refresh from production builds
+            if (env === 'production') {
+                const babelLoaderIndex = webpackConfig.module.rules[1].oneOf.findIndex(
+                    (rule) => rule.loader && rule.loader.includes('babel-loader')
+                );
+
+                if (babelLoaderIndex !== -1) {
+                    const babelLoader = webpackConfig.module.rules[1].oneOf[babelLoaderIndex];
+                    if (babelLoader.options && babelLoader.options.plugins) {
+                        babelLoader.options.plugins = babelLoader.options.plugins.filter(
+                            (plugin) => {
+                                if (typeof plugin === 'string') {
+                                    return !plugin.includes('react-refresh');
+                                }
+                                return true;
+                            }
+                        );
+                    }
+                }
+            }
+            return webpackConfig;
+        },
     },
 };
